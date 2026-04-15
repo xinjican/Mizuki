c9c18777983300ed6625bfff671522fd6166edc6
xinjican <xinjican@gmail.com>
2026-04-15 21:07:04 +0800

    fix: 配置base路径完成部署

--- src/config.ts
+++ src/config.ts
@@ -26,7 +26,7 @@ const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UT
 export const siteConfig: SiteConfig = {
 	title: "Mizuki",
 	subtitle: "One demo website",
-	siteURL: "https://mizuki.mysqil.com/", // 请替换为你的站点URL，以斜杠结尾
+	siteURL: "https://xinjican.github.io/Mizuki/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2026-04-15", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	timeZone: SITE_TIMEZONE,
