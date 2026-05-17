453976af6af8b456661e6d0781acc4fb82ae4c95
xinjican <xinjican@gmail.com>
2026-05-17 21:19:02 +0800

    更改lang与title

--- src/config.ts
+++ src/config.ts
@@ -21,11 +21,11 @@ import { LinkPreset } from "./types/config";
 // 移除i18n导入以避免循环依赖
 
 // 定义站点语言
-const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
+const SITE_LANG = "en"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UTC+8
 export const siteConfig: SiteConfig = {
-	title: "山茶的情书",
-	subtitle: "💌",
+	title: "aimer",
+	subtitle: "sy",
 	siteURL: "https://xinjican.github.io/Mizuki/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2026-04-15", // 站点开始运行日期，用于站点统计组件计算运行天数
 
